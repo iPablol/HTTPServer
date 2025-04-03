2f1ef19591d5bfa45363f56fe49a6d4cd3b022f2
iPablol <106445718+iPablol@users.noreply.github.com>
2025-04-03 13:27:36 +0200

    Added support for multiple connections

--- HTTPServer/Program.cs
+++ HTTPServer/Program.cs
@@ -17,21 +17,18 @@ namespace HTTPServer
         static async void Test()
         {
 
-            var ipEndPoint = new IPEndPoint(IPAddress.Loopback, 83);
+            var ipEndPoint = new IPEndPoint(IPAddress.Loopback, 80);
             TcpListener listener = new(ipEndPoint);
 
             try
             {
                 listener.Start();
-                using TcpClient handler = await listener.AcceptTcpClientAsync();
-                await using NetworkStream stream = handler.GetStream();
                 while (true)
                 {
-                    var buffer = new byte[1_024];
-                    int received = await stream.ReadAsync(buffer);
-
-                    string message = Encoding.UTF8.GetString(buffer, 0, received);
-                    Console.WriteLine($"Read message: \"{message}\"");
+                    if (listener.Pending())
+                    {
+                        HandleConnection(listener);
+                    }
                 }
             }
             finally
@@ -39,5 +36,19 @@ namespace HTTPServer
                 listener.Stop();
             }
         }
+
+        static async void HandleConnection(TcpListener listener)
+        {
+            using TcpClient handler = await listener.AcceptTcpClientAsync();
+            await using NetworkStream stream = handler.GetStream();
+            var buffer = new byte[1_024];
+            int received = await stream.ReadAsync(buffer);
+
+            string message = Encoding.UTF8.GetString(buffer, 0, received);
+            Console.WriteLine($"Read message: \"{message}\"");
+
+            await stream.WriteAsync(Encoding.UTF8.GetBytes($"Response to \"{message}\""));
+        }
     }
+    
 }
