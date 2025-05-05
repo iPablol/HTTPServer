a669cbd127162e08a68571235d883364da456c56
iPablol <106445718+iPablol@users.noreply.github.com>
2025-05-05 09:35:39 +0200

    Objectified the server

---
+++ HTTPServer/HTTPServer.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+
+internal class HTTPServer
+{
+	private IPEndPoint endPoint;
+
+	private TcpListener listener;
+
+	public bool stop;
+
+	public HTTPServer(IPAddress address, int port)
+	{
+		endPoint = new(address, port);
+		listener = new TcpListener(endPoint);
+	}
+
+	public async void Run()
+	{
+
+		var ipEndPoint = new IPEndPoint(IPAddress.Loopback, 80);
+		TcpListener listener = new(ipEndPoint);
+
+		try
+		{
+			listener.Start();
+			Console.WriteLine($"Listening on {endPoint.Address}:{endPoint.Port}");
+			while (!stop)
+			{
+				if (listener.Pending())
+				{
+					HandleConnection(listener);
+				}
+			}
+		}
+		finally
+		{
+			Console.WriteLine("Server closing...");
+			listener.Stop();
+		}
+	}
+
+	private async void HandleConnection(TcpListener listener)
+	{
+		using TcpClient handler = await listener.AcceptTcpClientAsync();
+		await using NetworkStream stream = handler.GetStream();
+		var buffer = new byte[1_024];
+		int received = await stream.ReadAsync(buffer);
+
+		string message = Encoding.UTF8.GetString(buffer, 0, received);
+		Console.WriteLine($"Read message:\n" +
+			$"-------------------------------" +
+			$" {message}" +
+			$"-------------------------------");
+
+		await stream.WriteAsync(Encoding.UTF8.GetBytes($"Response to \"{message}\""));
+	}
+
+}
+
--- HTTPServer/Program.cs
+++ HTTPServer/Program.cs
@@ -2,53 +2,17 @@
 using System.Net;
 using System.Text;
 
-namespace HTTPServer
+
+string[] quitCommands =
 {
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Test();
-            while (true)
-            {
-            }
-        }
-
-        static async void Test()
-        {
-
-            var ipEndPoint = new IPEndPoint(IPAddress.Loopback, 80);
-            TcpListener listener = new(ipEndPoint);
-
-            try
-            {
-                listener.Start();
-                while (true)
-                {
-                    if (listener.Pending())
-                    {
-                        HandleConnection(listener);
-                    }
-                }
-            }
-            finally
-            {
-                listener.Stop();
-            }
-        }
-
-        static async void HandleConnection(TcpListener listener)
-        {
-            using TcpClient handler = await listener.AcceptTcpClientAsync();
-            await using NetworkStream stream = handler.GetStream();
-            var buffer = new byte[1_024];
-            int received = await stream.ReadAsync(buffer);
-
-            string message = Encoding.UTF8.GetString(buffer, 0, received);
-            Console.WriteLine($"Read message: \"{message}\"");
-
-            await stream.WriteAsync(Encoding.UTF8.GetBytes($"Response to \"{message}\""));
-        }
-    }
-    
+	"quit", "q", "bye", "logout", "stop", "shutdown"
+};
+var server = new HTTPServer(IPAddress.Loopback, 80);
+new Task(server.Run).Start();
+string command = "";
+while (!quitCommands.Contains(command.ToLower()))
+{
+	command = Console.ReadLine();
 }
+
+server.stop = true;
