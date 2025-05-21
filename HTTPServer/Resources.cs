c105dcc5ad36def017c6b2d0a000584e03facb4f
iPablol <106445718+iPablol@users.noreply.github.com>
2025-05-21 22:00:43 +0200

    RACE CONDITIONS

--- HTTPServer/Resources.cs
+++ HTTPServer/Resources.cs
@@ -7,16 +7,17 @@ using System.Threading.Tasks;
 
 internal class Resources
 {
-	private List<Resource> resources = [new ("Primero"), new ("Segundo")];
+	private readonly Object resourcesListLock = new();
+	private List<Resource> resources = [new("Primero"), new("Segundo")];
 
 	public List<Resource> Get() => resources;
 
-	public void Add(Resource resource) => resources.Add(resource);
+	public void Add(Resource resource) { lock (resourcesListLock) { resources.Add(resource); } }
 
-	public void Edit(int pos, Resource resource) => resources[pos] = resource;
+	public void Edit(int pos, Resource resource) { lock (resourcesListLock) { resources[pos] = resource; } }
 
-	public bool Remove(Resource resource) => resources.Remove(resource);
-	public void Remove(int position) => resources.RemoveAt(position);
+	public bool Remove(Resource resource) { lock (resourcesListLock) { return resources.Remove(resource); } }
+	public void Remove(int position) { lock (resourcesListLock) { resources.RemoveAt(position); } }
 }
 
 internal record Resource(string Name);
